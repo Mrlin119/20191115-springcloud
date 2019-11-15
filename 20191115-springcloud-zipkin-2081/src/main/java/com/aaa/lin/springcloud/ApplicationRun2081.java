cc54447381356c02b6ce7484e5179c5dc741ef4d
Mrlin119 <56469897+Mrlin119@users.noreply.github.com>
2019-11-15 22:15:49 +0800

    提交springcloud的学习项目

---
+++ 20191115-springcloud-zipkin-2081/src/main/java/com/aaa/lin/springcloud/ApplicationRun2081.java
@@ -0,0 +1,15 @@
+package com.aaa.lin.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Company
+ * @Author Mr.Lin
+ * @Data Create in 2019/11/15 21:52
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApplicationRun2081 {
+}
